155d4b61e34277b7a61c2a6fe3301cd3bf203ba6
Amr Elkady <amr.elkady153@gmail.com>
2019-08-05 13:46:40 +0200

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -9,7 +9,10 @@ from datetime import datetime
 Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
-
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(1000))
 
 
 
